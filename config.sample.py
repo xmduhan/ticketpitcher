837bd10f05e538907f1b650ef409ccb17f2beedc
root <xmduhan>
2017-09-11 14:47:53 +0800

    ...

--- config.sample.py
+++ config.sample.py
@@ -4,7 +4,7 @@ Created on Fri Nov 14 22:09:56 2014
 
 @author: duhan
 """
-#arrayMapPath = r'/usr/lib/python2.7/dist-packages/ticketpitcher/data'
+#arrayMapPath = r'/usr/local/lib/python2.7/dist-packages/ticketpitcher/data/3'
 arrayMapPath = r'C:\Python27\Lib\site-packages\ticketpitcher\data'
 #tempPath = r'/tmp/'
 tempPath = 'd:\\temp\\'
